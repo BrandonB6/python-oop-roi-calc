b6c2105c236e72a98a56ca55c170c860c681f524
BrandonB6 <46602613+BrandonB6@users.noreply.github.com>
2023-12-27 09:37:46 -0500

    Add files via upload

--- oop_roi.py
+++ oop_roi.py
@@ -11,8 +11,34 @@ class Roi():
         self.income.append(((int(response1)) * (int(sub_response1))))
     
     def expensesCalc(self):
-        response2 = input("How much are your monthly expenses? ")
+        response2 = input("What is your monthly electric bill? ")
         self.expenses.append(int(response2))
+        response5 = input("What is your monthly insurance cost? ")
+        self.expenses.append(int(response5))
+        response6 = input("What is your monthly tax bill? ")
+        self.expenses.append(int(response6))
+        response7 = input("What is your monthly water bill? ")
+        self.expenses.append(int(response7))
+        response8 = input("What is your monthly sewer fee? ")
+        self.expenses.append(int(response8))
+        response9 = input("What is your monthly trash service cost? ")
+        self.expenses.append(int(response9))
+        response10 = input("What is your monthly gas bill? ")
+        self.expenses.append(int(response10))
+        response11 = input("What is your monthly HOA fee? ")
+        self.expenses.append(int(response11))
+        response12 = input("What is your monthly landscaping cost? ")
+        self.expenses.append(int(response12))
+        response13 = input("What is your monthly vacancy cost? ")
+        self.expenses.append(int(response13))
+        response14 = input("What is your monthly repairs cost? ")
+        self.expenses.append(int(response14))
+        response15 = input("What is your monthly capital expenses cost? ")
+        self.expenses.append(int(response15))
+        response16 = input("What is your monthly mortgage cost? ")
+        self.expenses.append(int(response16))
+        response17 = input("What is your monthly property management cost? ")
+        self.expenses.append(int(response17))
     
     def cashFlow(self):
         cash_flow = sum(self.income) - sum(self.expenses)
@@ -20,8 +46,9 @@ class Roi():
     
     def cashOnCash(self):
         response3 = input("How much was your down payment? ")
-        response4 = input("How much have you spent on your property for repairs, renovations, etc.? ")
-        self.investments.append(int(response3) + int(response4))
+        response4 = input("How much have you spent on your property for repairs, renreovations, etc.? ")
+        response19 = input("How much was your closing cost?")
+        self.investments.append(int(response3) + int(response4) + int(response19))
         cash_on_cash = ((((sum(self.income)) - (sum(self.expenses))) * 12) / sum(self.investments)) * 100
         cash_on_cash = round(cash_on_cash, 2)
         print(f'Your annual cash on cash ROI is {cash_on_cash}%')
